c8a3e6488de3997c99751af7f226f2ecb2e195f1
marilourubio04 <myrubio25@gmail.com>
2021-07-24 23:30:51 +0800

    Routes in web.php added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin/login',[AdminController::class,'login']);
+Route::get('admin',[AdminController::class,'index']);
+Route::post('admin/login',[AdminController::class,'submit_login']);
+Route::get('admin/logout',[AdminController::class,'logout']);
+ 
+// Department Resource
+Route::get('depart/{id}/delete',[DepartmentController::class,'destroy']);
+Route::resource('depart',DepartmentController::class);
+
+// Employee Resource
+Route::get('employee/{id}/delete',[EmployeeController::class,'destroy']);
+Route::resource('employee',EmployeeController::class);
+
